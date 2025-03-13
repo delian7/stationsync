b24cf2088d1b1eab78a08dcb788c588f10636938
Delian Petrov <delian7@gmail.com>
2025-03-13 04:23:34 +0000

    wip

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// filepath: /home/ubuntu/Desktop/eurotex/stationsync/tailwind.config.js
+module.exports = {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
