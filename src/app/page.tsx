027affa62c6b69f79d5802e9ef272038774092b1
Delian Petrov <delian7@gmail.com>
2025-03-31 15:40:07 +0000

    cleanup unused assets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,6 @@ import TableGrid from "./components/TableGrid";
 export default function Home() {
   return (
     <main className="flex flex-col bg-gray-100">
-      {/* <div>
-        <EmployeeList />
-      </div> */}
       <div>
         <TableGrid />
       </div>
